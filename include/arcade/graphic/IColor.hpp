285fa505a95ee6d86ebe106b4b227f1d1504c70f
Syl Eze <syleze.contact@gmail.com>
2019-03-09 16:46:35 +0100

    IColor: edit comments syntax

--- include/arcade/graphic/IColor.hpp
+++ include/arcade/graphic/IColor.hpp
@@ -8,7 +8,7 @@ namespace arcade::graphic
 {
 
 /**
- * class composed of 4 elements
+ * Class composed of 4 elements
  * red color as 8-bits unsigned integer
  * green color as 8-bits unsigned integer
  * blue color as 8-bits unsigned integer
@@ -22,39 +22,45 @@ public:
   virtual void copy(const IColor &color) = 0;
 
   /**
-   * get Color as a 32-bits unsigned integer
+   * Get Color as a 32-bits unsigned integer
    * color is given as [red, green, blue, alpha] each being 8-bits unsigned int in range [0, 255]
-   * @return
+   * @return color stored in an unsigned integer
    */
   virtual uint32_t getColor() const = 0;
+
   /**
-   * get Red color in range [0, 255]
-   * @return 8-bits unsigned int
+   * Get Red color in range [0, 255]
+   * @eturn 8-bits unsigned int
    */
   virtual uint8_t getRed() const = 0;
+
   /**
-   * get Green color in range [0, 255]
+   * Get Green color in range [0, 255]
    * @return 8-bits unsigned int
    */
   virtual uint8_t getGreen() const = 0;
+
   /**
-   * get Blue color in range [0, 255]
+   * Get Blue color in range [0, 255]
    * @return 8-bits unsigned int
    */
   virtual uint8_t getBlue() const = 0;
+
   /**
-   * get opacity level (alpha) in range [0, 255]
+   * Get opacity level (alpha) in range [0, 255]
    * @return 8-bits unsigned int
    */
   virtual uint8_t getAlpha() const = 0;
+
   /**
-   * set color of class
+   * Set color of class
    * @param red in range [0, 255]
    * @param green in range [0, 255]
    * @param blue in range [0, 255]
    * @param alpha in range [0, 255]
    */
   virtual void setColor(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) = 0;
+
   /**
    * Change color using a 32-bits unsigned integer
    * @param color RGBA levels in that order each in range [0, 255]
