6e67ad8895052ede44b21fe16bab02dc36d8342a
Syl Eze <syleze.contact@gmail.com>
2019-03-09 17:01:29 +0100

    Add Doxygen support, and add generated documentation

---
+++ doc/html/search/all_2.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['getalpha',['getAlpha',['../classarcade_1_1graphic_1_1_i_color.html#a44bd9427c1d58cbd49d64de03ffe86f2',1,'arcade::graphic::IColor']]],
+  ['getblue',['getBlue',['../classarcade_1_1graphic_1_1_i_color.html#a4d2755d998f9df75f9080f28e98b639b',1,'arcade::graphic::IColor']]],
+  ['getcharactersize',['getCharacterSize',['../classarcade_1_1graphic_1_1_i_text.html#a7eaa9a7674acefc362d2d1e6e07ae9dd',1,'arcade::graphic::IText']]],
+  ['getcolor',['getColor',['../classarcade_1_1graphic_1_1_i_color.html#af0e1bcabdae131d45e416cc8de726d98',1,'arcade::graphic::IColor::getColor()'],['../classarcade_1_1graphic_1_1_i_sprite.html#ac1d2f9de8387466857b06ad49bacae45',1,'arcade::graphic::ISprite::getColor()']]],
+  ['getfont',['getFont',['../classarcade_1_1graphic_1_1_i_text.html#a6370811bf285da57f7780843d6767ace',1,'arcade::graphic::IText']]],
+  ['getgreen',['getGreen',['../classarcade_1_1graphic_1_1_i_color.html#a415154c0e5a0cdd323de24a647cc09d5',1,'arcade::graphic::IColor']]],
+  ['getname',['getName',['../classarcade_1_1graphic_1_1_i_window.html#aaf984acf6c057878bc1c24134b897459',1,'arcade::graphic::IWindow']]],
+  ['getposition',['getPosition',['../classarcade_1_1graphic_1_1_i_sprite.html#a44047f1697c3bebbf808a239677d33e1',1,'arcade::graphic::ISprite::getPosition()'],['../classarcade_1_1graphic_1_1_i_text.html#a78de4816a5a2f5d8ea85ef30bf5e9a47',1,'arcade::graphic::IText::getPosition()']]],
+  ['getred',['getRed',['../classarcade_1_1graphic_1_1_i_color.html#a054e1eaae2aba7396d8d53e72008be71',1,'arcade::graphic::IColor']]],
+  ['getrotation',['getRotation',['../classarcade_1_1graphic_1_1_i_sprite.html#a2ea4c49f5da9b17a500074ab1378f7ff',1,'arcade::graphic::ISprite::getRotation()'],['../classarcade_1_1graphic_1_1_i_text.html#a814f9412043c8436ffa4614990d4c653',1,'arcade::graphic::IText::getRotation()']]],
+  ['getsize',['getSize',['../classarcade_1_1graphic_1_1_i_window.html#afe84df5e5cc47ba9d9e277d62f292684',1,'arcade::graphic::IWindow']]],
+  ['getstring',['getString',['../classarcade_1_1graphic_1_1_i_text.html#a15a5ef9ed961cd8f4c596f09ce79f694',1,'arcade::graphic::IText']]],
+  ['gettexture',['getTexture',['../classarcade_1_1graphic_1_1_i_sprite.html#a7e2a1006497060ad69d01055c2308967',1,'arcade::graphic::ISprite']]],
+  ['getx',['getX',['../classarcade_1_1graphic_1_1_i_vector2i.html#a63a56c6cc94bfa2269f1f863207c3407',1,'arcade::graphic::IVector2i::getX()'],['../classarcade_1_1graphic_1_1_i_vector2f.html#a04ebd90784d6aa580970d50643a7cd4c',1,'arcade::graphic::IVector2f::getX()']]],
+  ['gety',['getY',['../classarcade_1_1graphic_1_1_i_vector2i.html#a015a561871d04bf7ec5e1f900d56015e',1,'arcade::graphic::IVector2i::getY()'],['../classarcade_1_1graphic_1_1_i_vector2f.html#aaf18cfa7dca65b1b435d88198f68c39e',1,'arcade::graphic::IVector2f::getY()']]]
+];
---
+++ doc/html/search/all_8.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['_7eitext',['~IText',['../classarcade_1_1graphic_1_1_i_text.html#a311b4292400985ac1b8fe678cc9c639a',1,'arcade::graphic::IText']]],
+  ['_7eivector2f',['~IVector2f',['../classarcade_1_1graphic_1_1_i_vector2f.html#a3d7a2d030d2b13846fea3baf9a8c9280',1,'arcade::graphic::IVector2f']]],
+  ['_7eivector2i',['~IVector2i',['../classarcade_1_1graphic_1_1_i_vector2i.html#a6970b3bc7ba56ddab62e204da8c8ee61',1,'arcade::graphic::IVector2i']]],
+  ['_7eiwindow',['~IWindow',['../classarcade_1_1graphic_1_1_i_window.html#a444d8868d9aad4ee0d1a67433a30ffba',1,'arcade::graphic::IWindow']]]
+];
